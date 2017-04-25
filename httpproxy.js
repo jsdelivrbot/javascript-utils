fd05287084e7e695ecefe30a97aab2be9910ceda
Tom Fennelly <tom.fennelly@gmail.com>
2017-04-25 09:11:04 +0100

    Create httpproxy.js

---
+++ httpproxy.js
@@ -0,0 +1,68 @@
+/**
+ * Simple local proxy.
+ *
+ * Acts as a regular proxy, but also lets you swap in local resources, which is very useful for
+ * debugging JavaScript etc in a page in situations where you can't easily modify/rebuild the
+ * source JavaScript i.e. similar to www.requestly.in, but with local filesystem support
+ * and without having to install browser extensions.
+ *
+ * Default is to proxy port 18080 -> 8080
+ */
+
+//
+// MODIFY this array. Note you can add a 'headers' object too.
+//
+const proxyToLocals = [
+    {
+        from: '/scripts/hudson-behavior.js',
+        to: '/Users/tfennelly/projects/jenkins/war/src/main/webapp/scripts/hudson-behavior.js'
+    }
+];
+
+const proxyFromPort = 18080;
+const proxyToPort = 8080;
+
+const mimetypes = {
+    '.js': 'application/javascript',
+    '.css': 'text/css'
+};
+
+const http = require('http');
+const httpProxy = require('http-proxy');
+const fs = require('fs');
+const path = require('path');
+
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer(function(req, res) {
+
+    for (let i = 0; i < proxyToLocals.length; i++) {
+        const proxyToLocal = proxyToLocals[i];
+
+        if (req.url.endsWith(proxyToLocal.from)) {
+            console.log(`Applying proxyToLocal: ${proxyToLocal.from} -> ${proxyToLocal.to}`);
+
+            const body = fs.readFileSync(proxyToLocal.to, "utf-8");
+            const headers = {
+                'Server': `Local-resource-proxy: ${__filename}`,
+                'Content-Type': mimetypes[path.extname(proxyToLocal.to)],
+                'Content-Length': Buffer.byteLength(body)
+            };
+
+            if (proxyToLocal.headers) {
+                Object.assign(headers, proxyToLocal.headers);
+            }
+
+            res.writeHead(200, headers);
+            res.write(body);
+            res.end('ok');
+
+            return;
+        }
+    }
+
+    proxy.web(req, res, { target: `http://localhost:${proxyToPort}` });
+});
+
+server.listen(proxyFromPort);
+
+console.log(`Proxying port ${proxyFromPort} to localhost:${proxyToPort}`);
